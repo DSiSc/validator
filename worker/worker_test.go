12d604a2cf8b30ec9cec2e655883724b07d94fe5
walterBishop <walterkangluo@gmail.com>
2018-08-31 17:17:45 +0800

    optimise# Add test cases to worker.

--- worker/worker_test.go
+++ worker/worker_test.go
@@ -1,14 +1,23 @@
 package worker
 
 import (
+	"github.com/DSiSc/craft/types"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-func Test_NewWorker(t *testing.T) {
+func TestNewWorker(t *testing.T) {
 	assert := assert.New(t)
 	var worker = NewWorker(nil, nil)
 	assert.NotNil(worker)
 	assert.Nil(worker.block)
 	assert.Nil(worker.chain)
+}
+
+func TestGetTxsRoot(t *testing.T) {
+	var trxs = make([]*types.Transaction, 0)
+	trx := new(types.Transaction)
+	trxs = append(trxs, trx)
+	hash := GetTxsRoot(trxs)
+	assert.NotNil(t, hash)
 }
